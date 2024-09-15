4fb65df5f86ad6ccb4fe2cfbde48ad7fa2672d61
aSchemitt <alison.schemitt@edu.pucrs.br>
2024-09-15 16:59:33 -0300

    created responses and mapper

---
+++ src/main/java/com/pes/centro_distribuicao_ms/controller/CDResponse.java
@@ -0,0 +1,14 @@
+package com.pes.centro_distribuicao_ms.controller;
+
+import lombok.Data;
+
+@Data
+public class CDResponse {
+
+    private String name;
+    private String phone;
+    private String email;
+    private String code;
+
+    private AddressResponse address;
+}
---
+++ src/main/java/com/pes/centro_distribuicao_ms/mapper/CDMapper.java
@@ -0,0 +1,35 @@
+package com.pes.centro_distribuicao_ms.mapper;
+
+import com.pes.centro_distribuicao_ms.controller.AddressResponse;
+import com.pes.centro_distribuicao_ms.controller.CDResponse;
+import com.pes.centro_distribuicao_ms.domain.CentroDistribuicao;
+
+public class CDMapper {
+    
+    public static CDResponse toResponse(CentroDistribuicao cd){
+        CDResponse response = new CDResponse();
+        AddressResponse addressResponse = new AddressResponse();
+
+        response.setCode(cd.getCode());
+        response.setEmail(cd.getEmail());
+        response.setName(cd.getName());
+        response.setPhone(cd.getPhone());
+
+        addressResponse.setCountry(cd.getAddress().getCountry());
+        addressResponse.setState(cd.getAddress().getState());
+        addressResponse.setCity(cd.getAddress().getCity());
+        addressResponse.setNeighborhood(cd.getAddress().getNeighborhood());
+        addressResponse.setStreet(cd.getAddress().getStreet());
+        addressResponse.setNumber(cd.getAddress().getNumber());
+
+        response.setAddress(addressResponse);
+
+
+
+
+
+
+
+        return response;
+    }
+}
