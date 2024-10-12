8ad21a7c4d7fc6f723aa7e04050c0495945b50c8
cassiano-flores <cassiano.flores2424@gmail.com>
2024-10-12 15:13:10 -0300

    update swagger configs

--- src/main/java/com/pes/centro_distribuicao_ms/configs/swagger/OpenApiConfig.java
+++ src/main/java/com/pes/centro_distribuicao_ms/configs/swagger/OpenApiConfig.java
@@ -2,21 +2,19 @@ package com.pes.centro_distribuicao_ms.configs.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-    info = @Info(
-        title = "API de Centro de Distribuição",
-        version = "1.0.0",
-        description = "API para os serviços de centro de distribuição de donativos"
-))
+        info = @Info(
+                title = "API de Centro de Distribuição",
+                version = "1.0.0",
+                description = "API para os serviços de centro de distribuição de donativos"
+        ))
 public class OpenApiConfig {
     // Outras configurações específicas podem ser adicionadas aqui.
-    
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
--- src/main/java/com/pes/centro_distribuicao_ms/controller/CDController.java
+++ src/main/java/com/pes/centro_distribuicao_ms/controller/CDController.java
@@ -1,32 +1,18 @@
 package com.pes.centro_distribuicao_ms.controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
-
 import com.pes.centro_distribuicao_ms.controller.request.IncludeCDRequest;
 import com.pes.centro_distribuicao_ms.controller.response.CDCodeResponse;
 import com.pes.centro_distribuicao_ms.controller.response.CDResponse;
 import com.pes.centro_distribuicao_ms.service.CDService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,44 +20,38 @@ import java.util.List;
 @RequestMapping("/v1/cds")
 @CrossOrigin(origins = "*")
 public class CDController {
-    
+
     @Autowired
     private CDService cdService;
 
-    //gets
-
-    @Operation(summary = "Get a CD or a full list of CDs.",description = "Return a specific CD by name or ID or the full list of the CDs.")
+    @Operation(summary = "Get a CD or a full list of CDs.", description = "Return a specific CD by name or ID or the full list of the CDs.")
     @ApiResponses(value = {
-    @ApiResponse(responseCode = "200", description = "CD found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CDResponse.class))),
-    @ApiResponse(responseCode = "400", description = "Invalid parameters", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": \"Invalid parameters\"}"))),
-    @ApiResponse(responseCode = "404", description = "CD not found", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": \"CD not found\"}"))),
+            @ApiResponse(responseCode = "200", description = "CD found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CDResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": \"Invalid parameters\"}"))),
+            @ApiResponse(responseCode = "404", description = "CD not found", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": \"CD not found\"}"))),
     })
     @GetMapping
-    public ResponseEntity<?> getCD(@RequestParam(required = false) Long codCD,@RequestParam(required = false) String nameCD){
-        
+    public ResponseEntity<?> getCD(@RequestParam(required = false) Long codCD, @RequestParam(required = false) String nameCD) {
+
         if (codCD != null) {
             CDResponse response = cdService.getCDByID(codCD);
             return ResponseEntity.ok(response);
         } else if (nameCD != null) {
             CDResponse response = cdService.getCDByName(nameCD);
             return ResponseEntity.ok(response);
-        } else{
+        } else {
             List<CDResponse> responseList = cdService.getAllCDs();
             return ResponseEntity.ok(responseList);
         }
     }
 
-    //posts
-
-    @Operation(summary = "Create a new CD.",description = "Create a new CD using the given parameters.")
+    @Operation(summary = "Create a new CD.", description = "Create a new CD using the given parameters.")
     @ApiResponses(value = {
-    @ApiResponse(responseCode = "201", description = "CD created", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CDCodeResponse.class))),
-    @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": [\"Phone is required\",\n" + "\"Neighborhood is required\"]}")))
+            @ApiResponse(responseCode = "201", description = "CD created", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CDCodeResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": [\"Phone is required\",\n" + "\"Neighborhood is required\"]}")))
     })
     @PostMapping
-    public CDCodeResponse include(@Valid @RequestBody IncludeCDRequest request){
+    public CDCodeResponse include(@Valid @RequestBody IncludeCDRequest request) {
         return cdService.include(request);
     }
-
-    
 }
