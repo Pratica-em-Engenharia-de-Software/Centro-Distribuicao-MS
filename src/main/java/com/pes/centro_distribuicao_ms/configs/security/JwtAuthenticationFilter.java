952120bcbb3fea97cabd57d5e4aa241334a46ee4
aSchemitt <alison.schemitt@edu.pucrs.br>
2024-12-05 16:42:11 -0300

    added specific role check

--- src/main/java/com/pes/centro_distribuicao_ms/configs/security/JwtAuthenticationFilter.java
+++ src/main/java/com/pes/centro_distribuicao_ms/configs/security/JwtAuthenticationFilter.java
@@ -1,11 +1,14 @@
 package com.pes.centro_distribuicao_ms.configs.security;
 
 import java.io.IOException;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
 
@@ -19,6 +22,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtTokenValidator jwtTokenValidator;
   private final JwtUtil jwtUtil;
+
+  private final Set<String> validRoles = Set.of("superadmin","adminAbrigo", "adminCD", "voluntario");
+  private final Set<String> validRolesGet = Set.of("superadmin", "adminCD");
+  private final Set<String> validRolesPost = Set.of("superadmin");
 
   @Autowired
   public JwtAuthenticationFilter(JwtTokenValidator jwtTokenValidator, JwtUtil jwtUtil) {
@@ -36,12 +43,35 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     if (token != null) {
       try {
         // Valida o token JWT
+        
         DecodedJWT decodedJWT = jwtTokenValidator.validateToken(token);
-
+        
         // Extraindo informações do token
         String userId = decodedJWT.getSubject();
         String email = decodedJWT.getClaim("email").asString();
         String role = decodedJWT.getClaim("role").asString();
+
+        System.out.println("role: "+role);
+        if (validRoles.contains(role)) {
+            System.out.println("role valido");
+        } else {
+            System.out.println("role invalido");
+            throw new ResponseStatusException(FORBIDDEN);
+        }
+        
+        // Restrição de acesso baseada no método HTTP e endpoint 
+        String requestURI = request.getRequestURI(); String method = request.getMethod();
+        
+        if (requestURI.equals("/v1/cds") && (method.equals("GET"))) {
+            if (!validRolesGet.contains(role)) {
+                throw new ResponseStatusException(FORBIDDEN);
+            }
+        }
+        if (requestURI.equals("/v1/cds") && (method.equals("POST"))) {
+            if (!validRolesPost.contains(role)) {
+                throw new ResponseStatusException(FORBIDDEN);
+            }
+        }
 
         // Configura o JwtUtil com os detalhes
         jwtUtil.setJwtDetails(token, userId, email, role);
